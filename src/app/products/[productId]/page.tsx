b3b6dfaff33cb4bcfba3e351be2bfccb71dd5e4c
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-10 21:17:53 +0530

    dynamic generateMetadata function configuration

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,32 @@
-export default function ProductDetails({ params }: {
-  params : {productId : string}
+import { Metadata } from "next";
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Android ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({
+  params,
+}: {
+  params: { productId: string };
 }) {
   return (
     <>
-      <h1>Details about product {params.productId }</h1>
-      
+      <h1>Details about product {params.productId}</h1>
     </>
   );
 }
