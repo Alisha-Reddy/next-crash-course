674baedaa2744f80898986f04336c7cc7862118d
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-11 11:12:07 +0530

    template

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import "./styles.css"
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -15,9 +16,14 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
+    const pathname = usePathname();
+    const [input, setInput] = useState("")
   return (
-    <div>
+      <div>
+          <div>
+              <label>Input</label>
+              <input type="text" value = {input} onChange={(e) => setInput(e.target.value)} />
+          </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
         return (
