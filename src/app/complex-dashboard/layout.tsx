b6f5285beb9490a88d5a9fd18db51f8bc264408b
Alisha-Reddy <kondapualishareddy@gmail.com>
2024-08-18 10:59:30 +0530

    unmatched routes

--- src/app/complex-dashboard/layout.tsx
+++ src/app/complex-dashboard/layout.tsx
@@ -3,22 +3,27 @@ export default function DAshboardLayout({
   users,
   revenue,
   notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
-    }) {
-    return (
-      <div>
-        <div>{children}</div>
-        <div style={{ display: "flex" }}>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <div>{users}</div>
-            <div>{revenue}</div>
-          </div>
-          <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+  login: React.ReactNode;
+  }) {
+  const isLggedIn = true;
+  return isLggedIn ?(
+    <div>
+      <div>{children}</div>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
         </div>
+        <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
-    );
+    </div>
+  ) : (
+      login
+  );
 }
